c77801e3c21ae6c2b2c27c9040f47fff61b80868
Colin Ayres <CAyres7@gmail.com>
2022-04-12 21:45:42 -0400

    Shows path that knight takes; need to fix bugs in algorithm for calculating path

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -28,14 +28,14 @@
             # We're moving 1 step away from the base node, so at the new node we'll increment the distance by 1
             distance[new_node[0]][new_node[1]][:distance] = distance[base_node.x_position][base_node.y_position][:distance] + 1
             # We'll repeat this process for the next node. Eventually we end up with a board full of numbers that represent node distances from the starting position
-            queue << Knight.new(move)
-          end      
+            queue << Knight.new(new_node)
+          end
         end
       end
 
     # In the next step, we iterate through the distance array and track the nodes with the lowest distance values
     shortest_seeker = Knight.new(start)
-    shortest_path = 7
+    shortest_path = 63
     current_node = nil
     # Until our iterator reaches the final node...
     until current_node == final
@@ -46,23 +46,24 @@
           shortest_path = distance[new_node[0]][new_node[1]][:distance]
           current_node = new_node
         end
-      solution << current_node
-      shortest_seeker = Knight.new(current_node)
       end
+    solution << current_node
+    shortest_seeker = Knight.new(current_node)
+      
     end
     print_travails(solution, start, final)
   end
 
-  def print_travails
+  def print_travails(solution, start, final)
     puts "The shortest path from #{start} to #{final} is #{solution.inspect}."
 	print "\nThe Knight starts at #{start}.\n\n"
     knight_travails = Gameboard.new
 	knight_travails.add_knight(solution.shift)
-	knight_travails.display
+	knight_travails.display_board
 	solution.each_with_index do |new_node, index| 
 		knight_travails.move_knight(new_node)
 		print "\nThen it moves to #{solution[index]}.\n\n"
-		knight_travails.display
+		knight_travails.display_board
     end
   end
 #end
